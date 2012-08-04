9178594298dbf216a241c7dbfd5ecd2889588133
Barney Parker <github@theparkerwilmottproject.co.uk>
2012-08-04 16:10:35 +0100

    Interrupt and Power Saving
    Added interrupt driven wake-up functionality, and timed power-down
    
    Some general code clean-up

--- Arduino_Countdown_Timer/Arduino_Countdown_Timer.ino
+++ Arduino_Countdown_Timer/Arduino_Countdown_Timer.ino
@@ -1,4 +1,6 @@
 // include the library code:
+#include <avr/sleep.h>
+#include <avr/power.h>
 #include <LiquidCrystal.h>
 #include <Wire.h>
 #include <RTClib.h>
@@ -9,29 +11,31 @@ LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 //Initialise the RTC Object
 RTC_DS1307 RTC;
 
-//Button Var
-volatile int state = 0;
+//update vars
+long nextLCDUpdate = 0;
+long powerOffTime = 0;
 
 //Backlight pin
 int backlight = 7;
+
+void setPowerOffTime() {
+  powerOffTime = millis() + 10000;//5 sec delay
+}
 
 void setup(){
   //Set up Serial Output
   Serial.begin(9600);
   
-  // set up the LCD's number of columns and rows: 
+  // set up the LCD's number of columns and rows and enable (inc blacklight)
   lcd.begin(16, 2);
-  lcd.clear();
+  LCDEnable();
   
-  //Configure the button interrupt
-  attachInterrupt(0, buttonHandler, RISING);
+  //make sure we're not going to time out too quickly
+  setPowerOffTime();
   
   //set Digital pin 7 to OUTPUT
   pinMode(7, OUTPUT);
 
-  //call the buttonHandler to switch on the LCD
-  buttonHandler();
-  
   //Set up Wire
   Wire.begin();
   
@@ -46,34 +50,58 @@ void setup(){
   }
 }
 
-void buttonHandler() {
-  state = ((state == 0) ? 1 : 0);
-  
-  if(state == 0) {
-    lcd.noDisplay();        //turn off the LCD display
-    digitalWrite(7, LOW);  //turn the backlight off
-  } else {
-    lcd.display();          //turn on the LCD display
-    digitalWrite(7, HIGH);  //turn the backlight on
-  }
-    
-  Serial.print("Button State = ");  
-  Serial.println(state);
+void LCDEnable() {
+  lcd.display();          //turn on the LCD display
+  digitalWrite(7, HIGH);  //turn the backlight on
+}
+
+void LCDDisable() {
+  lcd.noDisplay();        //turn off the LCD display
+  digitalWrite(7, LOW);   //turn the backlight off
+}
+
+void buttonISR() {
+  detachInterrupt(0);   //detach interrupts
+  sleep_disable();      //disable powersaving
+  LCDEnable();          //enable the LCD
+  setPowerOffTime();    //reset the power off time
+  nextLCDUpdate = 0;    //update the LCD immediately
 }
 
 void loop()
 {
+
+  if(millis() > nextLCDUpdate) {
+    updateLCD();
+    nextLCDUpdate = millis() + 1000;  //set the next update time
+  }
+  
+  if(millis() > powerOffTime) {
+    //attach the interrupt function
+    attachInterrupt(0, buttonISR, RISING);//TODO Should Be LOW - do we need to rewire? - was RISING
+    
+    //disable the LCD to save power
+    LCDDisable();
+    
+    //enable power saving
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+    sleep_mode();
+  }
+}
+
+void updateLCD() {
   //get the RTC time
   DateTime now = RTC.now();
   DateTime hol(2012, 10, 15, 8, 10, 0);
-  
+
+  //calculate the remaining time  
   unsigned long remaining = hol.unixtime() - now.unixtime();
 
+  //do the LCD update
   lcd.clear();
+  lcd.setCursor(0,0);
 
-  lcd.setCursor(0,0)  ;
-  //Display the time/date on the LCD
-  //lcdTime(now);
   lcd.print("Time to Holidays:");
 
   lcd.setCursor(0,1);
@@ -84,28 +112,4 @@ void loop()
   lcd.print(((remaining % 84600L) % 3600) / 60);
   lcd.print(":");
   lcd.print(((remaining % 84600L) % 3600) % 60);
-  
-//  lcd.print(remaining);
-//  lcd.print(":");
-
-  //delay a bit...  
-  delay(300);  
-}
-
-void lcdDate(DateTime d) {
-  //Display the date on the LCD
-  lcd.print(d.day(), DEC);
-  lcd.print('/');
-  lcd.print(d.month(), DEC);
-  lcd.print('/');  
-  lcd.print(d.year(), DEC);
-}
-
-void lcdTime(DateTime d) {
-  //Display the time on the LCD
-  lcd.print(d.hour(), DEC);
-  lcd.print(':');
-  lcd.print(d.minute(), DEC);
-  lcd.print(':');
-  lcd.print(d.second(), DEC);
 }
